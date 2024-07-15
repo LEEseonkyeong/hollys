ba6cf2c1dc460b69036d11a2c171f0856bc1ca90
LEEseonkyeong <o0osk@kakao.com>
2024-07-15 17:45:34 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(function () {
+
+    // menu 영역
+    $(".main > li, .sub_bg").hover(function () {
+        $(".sub").stop().show();
+        $(".sub_bg").stop().slideDown();
+    }, function () {
+        $(".sub").stop().hide();
+        $(".sub_bg").stop().slideUp();
+    }) // hover
+
+    //slick
+    $(".move").slick({
+        autoplay: true,
+        autoplaySpeed: 2000,
+        dots: true,
+        arrows: false,
+        adaptiveHeight: true,
+        fade:true
+    }); //slick
+
+}) // jQuery
